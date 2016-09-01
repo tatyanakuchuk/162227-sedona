486b1e3d5eeed3c1250f46ab5980dd6deb6156a5
tatyanakuchuk <tatyana8787@gmail.com>
2016-09-01 18:22:22 +0200

    Прогрессивное улучшение

--- js/popup.js
+++ js/popup.js
@@ -8,7 +8,6 @@ var count_child = popup.querySelector("#children");
 var storage_count_adult = localStorage.getItem("count_adult");
 var storage_count_child = localStorage.getItem("count_child");
 
-
 search.addEventListener("click", function(event){
 	event.preventDefault();
 	popup.classList.toggle("search_form_show");
@@ -20,8 +19,15 @@ search.addEventListener("click", function(event){
 		count_child.value = storage_count_child;
 	}
 });
+	
+	date_arrival.removeAttribute("required");
+	date_departure.removeAttribute("required");
+	count_adult.removeAttribute("required");
+	count_child.removeAttribute("required");
 
 form.addEventListener("submit", function(event){
+	
+
 	if (!date_arrival.value || !date_departure.value || !count_adult.value || !count_child.value){
 		event.preventDefault();
 		popup.classList.add("search_form_error");
